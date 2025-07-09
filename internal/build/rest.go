b7ab46b63747dc8d92dea0e049c6cabc3b3d02f6
kbatuev <kbatuev@htc-cs.ru>
2025-07-09 12:08:37 +0400

    Добавление main.html

--- internal/build/rest.go
+++ internal/build/rest.go
@@ -2,6 +2,7 @@ package build
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net"
 	"net/http"
@@ -65,7 +66,7 @@ func (b *Builder) registerHandlers(db *pgxpool.Pool) error {
 	operationHandler := server.OperationHandler
 	accountHandler := server.AccountHandler
 	userHandler := server.UserHandler
-
+	router.HandleFunc("/", b.homeHandler).Methods(http.MethodGet)
 	apiRoute.HandleFunc("/operations/commission", operationHandler.CommissionFromBroker).Methods(http.MethodPost)
 	apiRoute.HandleFunc("/operations/load", operationHandler.LoadOperations).Methods(http.MethodPost)
 	apiRoute.HandleFunc("/accounts/load", accountHandler.LoadAccounts).Methods(http.MethodGet)
@@ -75,4 +76,20 @@ func (b *Builder) registerHandlers(db *pgxpool.Pool) error {
 	apiRoute.HandleFunc("/user/create", userHandler.CreateUser).Methods(http.MethodPost)
 
 	return nil
+}
+
+func (b *Builder) homeHandler(w http.ResponseWriter, r *http.Request) {
+	b.renderTemplate(w, "main.html", nil)
+}
+
+func (b *Builder) renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	t, err := template.ParseFiles("app/templates/" + tmpl)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
